4e4778d508a70558553ce1506f23c9589927b127
downkhg <downkhg@gmail.com>
2021-05-25 19:48:25 +0900

    케릭터 이동과  물리, 충돌체크

--- Unity2D/PlatfomerUnity2D/Assets/Scripts/Dynamic.cs
+++ Unity2D/PlatfomerUnity2D/Assets/Scripts/Dynamic.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class Dynamic : MonoBehaviour
 {
+    public float Speed;
     // Start is called before the first frame update
     void Start()
     {
-        
+        //transform.position += Vector3.right;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position += Vector3.right * Time.deltaTime;
+        //왼쪽, 위,아래 모두 추가하기
+        if (Input.GetKey(KeyCode.RightArrow))
+            transform.position += Vector3.right * Speed * Time.deltaTime;
+        if (Input.GetKey(KeyCode.LeftArrow))
+            transform.position += Vector3.left * Speed * Time.deltaTime;
+        if (Input.GetKey(KeyCode.UpArrow))
+            transform.position += Vector3.up * Speed * Time.deltaTime;
+        if (Input.GetKey(KeyCode.DownArrow))
+            transform.position += Vector3.down * Speed * Time.deltaTime;
     }
 }
