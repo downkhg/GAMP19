f0bcf2615e6dd1806164eb9c460306888cc12e09
downkhg <downkhg@gmail.com>
2021-05-26 19:40:41 +0900

    점프와 강체와 충돌체크

--- Unity2D/PlatfomerUnity2D/Assets/Scripts/Dynamic.cs
+++ Unity2D/PlatfomerUnity2D/Assets/Scripts/Dynamic.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class Dynamic : MonoBehaviour
 {
     public float Speed;
+    public float JumpPower;
+    public bool isJump = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,9 +21,31 @@ public class Dynamic : MonoBehaviour
             transform.position += Vector3.right * Speed * Time.deltaTime;
         if (Input.GetKey(KeyCode.LeftArrow))
             transform.position += Vector3.left * Speed * Time.deltaTime;
-        if (Input.GetKey(KeyCode.UpArrow))
-            transform.position += Vector3.up * Speed * Time.deltaTime;
+        
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            if (isJump == false)
+            {
+                Rigidbody2D rigidbody = GetComponent<Rigidbody2D>();
+                rigidbody.AddForce(Vector3.up * JumpPower);
+                isJump = true;
+                //transform.position += Vector3.up * JumpPower * Time.deltaTime;
+            }
+        }
+        
         if (Input.GetKey(KeyCode.DownArrow))
             transform.position += Vector3.down * Speed * Time.deltaTime;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        isJump = false;
+        Debug.Log("OnCollisionEnter2D:" + collision.gameObject.name);
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        //isJump = true; //땅에서 나갔다고 해서 점프인가?
+        Debug.Log("OnCollisionExit2D:" + collision.gameObject.name);
     }
 }
