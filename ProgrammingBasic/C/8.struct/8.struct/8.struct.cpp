632dad47ba5f5da864eed91d92470a678e989aab
downkhg <downkhg@gmail.com>
2021-08-12 19:50:23 +0900

    공용체

--- ProgrammingBasic/C/8.struct/8.struct/8.struct.cpp
+++ ProgrammingBasic/C/8.struct/8.struct/8.struct.cpp
@@ -46,6 +46,7 @@ void PrintPlayerPtr(SPlayer* pPlayer)
 {
 	printf("PrintPlayerPtr:%d\n", pPlayer);
 	printf("Name:%s\n", pPlayer->strName);
+	//printf("HP:%d\n", (*pPlayer).nMP);
 	printf("HP:%d\n", pPlayer->nMP);
 	printf("MP:%d\n", pPlayer->nHP);
 	printf("Atk:%d\n", pPlayer->nAtk);
@@ -81,6 +82,34 @@ void StructMain()
 		PrintPlayer(arrPlayer[i]);
 		PrintPlayerPtr(&arrPlayer[i]);
 	}
+}
+union Matrix4x4 {
+	struct {
+		float        _11, _12, _13, _14;
+		float        _21, _22, _23, _24;
+		float        _31, _32, _33, _34;
+		float        _41, _42, _43, _44;
+
+	};
+	float m[4][4];
+};
+
+void UninonTestMain()
+{
+	Matrix4x4 mat;
+	int nCount = 0;
+	for (int y = 0; y < 4; y++)
+	{
+		for (int x = 0; x < 4; x++)
+		{
+			mat.m[y][x] = nCount;
+			nCount++;
+		}
+	}
+	printf("mat._11:%f\n", mat._11);
+	printf("mat._21:%f\n", mat._21);
+	printf("mat._31:%f\n", mat._31);
+	printf("mat._41:%f\n", mat._41);
 }
 
 void main()
