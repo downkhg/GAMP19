e7392856f02f9b77774a7586c8fb88c27b8974a9
downkhg <downkhg@gmail.com>
2021-09-07 11:18:36 +0900

    DFS큐 순회

--- ProgrammingBasic/DataStruct/3.Graph/Graph/Graph.cpp
+++ ProgrammingBasic/DataStruct/3.Graph/Graph/Graph.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <list>
 #include <vector>
+#include <stack>
 
 using namespace std;
 
@@ -8,17 +9,25 @@ struct SNode
 {
 	char cData;
 	bool bVisit = false;
+	list<SNode*>::iterator itChild; //
 	list<SNode*> listNext;
 };
 
 bool MakeChild(SNode* pParent, SNode* pChild)
 {
 	if (pParent != NULL)
+	{
 		pParent->listNext.push_back(pChild);
+		if (pParent->listNext.size() == 1)
+		{
+			pParent->itChild = pParent->listNext.begin();
+			cout << pParent->cData << " itChild:" << (*pParent->itChild)->cData << endl;
+		}
+	}
 	return false;
 }
 
-void TraverseDFS(SNode* pNode)
+void TraverseDFSRecursion(SNode* pNode)
 {
 	if (pNode->bVisit) return;
 	cout << pNode->cData << endl;
@@ -27,14 +36,77 @@ void TraverseDFS(SNode* pNode)
 	for (; it != pNode->listNext.end(); it++)
 	{
 		SNode* pNode = *it;
-		TraverseDFS(pNode);
+		TraverseDFSRecursion(pNode);
 	}
+}
+
+
+SNode* VisitDFS(SNode* pNode, stack<SNode*>& visit)
+{
+	SNode* pNext = NULL;
+	if (pNode)
+	{
+		if (pNode->bVisit == false)
+		{
+			//cout << "Visit:" << pNode->cData << endl;
+			cout << pNode->cData << endl;
+			visit.push(pNode);
+			pNode->bVisit = true;
+		}
+		else
+		{
+			//cout << "Revisit:" << pNode->cData << endl;
+		}
+		//list<SNode*>::iterator it = pNode->itChild;
+		if (pNode->listNext.size() > 0)
+		{
+			if (pNode->itChild != pNode->listNext.end())
+			{
+				pNext = *pNode->itChild;
+				while (pNext != NULL && pNext->bVisit == true)
+				{
+					if (pNode->itChild != pNode->listNext.end())
+					{
+						//cout << "Revisit!" << (*pNode->itChild)->cData << endl;
+						pNext = *pNode->itChild;
+						pNode->itChild++;
+					}
+					else
+					{
+						//cout << "Visit Complete!" << pNode->cData << endl;
+						pNext = NULL;
+					}
+				}
+			}
+		}
+	}
+	else
+	{
+		//cout << "Visit Complete! " << visit.top()->cData << endl;
+		visit.pop();
+		if(!visit.empty())
+			pNext = visit.top();
+	}
+	return pNext;
+}
+
+void TraverDFSStack(SNode* pNode)
+{
+	stack<SNode*> visit;
+	do
+	{
+		pNode = VisitDFS(pNode,visit);
+	} 	
+	while (!visit.empty());
 }
 
 void TraverseReset(vector<SNode*>& vec)
 {
 	for (int i = 0; i < vec.size(); i++)
+	{
 		vec[i]->bVisit = false;
+		vec[i]->itChild = vec[i]->listNext.begin();
+	}
 }
 
 enum E_NODE { A, B, C, D, E, F, G,H, MAX};
@@ -77,7 +149,8 @@ void main()
 
 	//MakeChild(vecNodes[H], NULL);
 
-	TraverseDFS(vecNodes[A]);
+	//TraverseDFSRecursion(vecNodes[A]);
+	TraverDFSStack(vecNodes[A]);
 	TraverseReset(vecNodes);
 
 	for (int i = 0; i < vecNodes.size(); i++)
