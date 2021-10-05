27232ab01e21efda6a4e01e658c21f6a9db9d9df
downkhg <downkhg@gmail.com>
2021-10-05 19:41:49 +0900

    계층상속과 다이아몬드상속의 문제점 클래스의 올바른 설계방법

--- ProgrammingBasic/C++/3.1.Inheritance/3.1.Inheritance/InheritaceCar.h
+++ ProgrammingBasic/C++/3.1.Inheritance/3.1.Inheritance/InheritaceCar.h
@@ -49,7 +49,36 @@ namespace Inheritance
 		}
 	};
 
-	class CAutoBike : public CVihicle
+	class CEnginer : public CVihicle
+	{
+		int m_nEngine;
+	public:
+		CEnginer(int seat, int engine):CVihicle(seat)
+		{
+			m_nEngine = engine;
+			cout << "CEnginer[" << this << "]" << endl;
+		}
+		~CEnginer()
+		{
+			cout << "~CEnginer[" << this << "]" << endl;
+		}
+	};
+
+	class CAutoBike : public CBike, public CEnginer
+	{
+	public:
+		CAutoBike(int seat, int engine):CBike(seat),CEnginer(seat, engine)
+		{
+			cout << "CAutoBike[" << this << "]" << endl;
+		}
+		~CAutoBike()
+		{
+			cout << "~CAutoBike[" << this << "]" << endl;
+		}
+	};
+
+
+	/*class CAutoBike : public CVihicle
 	{
 		int m_nEngine;
 	public:
@@ -62,26 +91,26 @@ namespace Inheritance
 		{
 			cout << "~CAutoBike[" << this << "]" << endl;
 		}
-	};
+	};*/
 
-	class CBus : public CAutoBike
-	{
-	public:
-		CBus(int seat, int engine):CAutoBike(seat, engine)
-		{
-			cout << "CBus[" << this << "]" << endl;
-		}
-		~CBus()
-		{
-			cout << "~CBus[" << this << "]" << endl;
-		}
-	};
-
-	class CTruck : public CBus
+	//class CBus : public CEnginer
+	//{
+	//public:
+	//	CBus(int seat, int engine):CEnginer(seat, engine)
+	//	{
+	//		cout << "CBus[" << this << "]" << endl;
+	//	}
+	//	~CBus()
+	//	{
+	//		cout << "~CBus[" << this << "]" << endl;
+	//	}
+	//};
+	//계층상속: 부모가 있는 클래스를 상속받는것.
+	class CTruck : public CEnginer
 	{
 		int m_nWeight;
 	public:
-		CTruck(int seat, int engine, int weight):CBus(seat, engine)
+		CTruck(int seat, int engine, int weight):CEnginer(seat, engine)
 		{
 			m_nWeight = weight;
 			cout << "CTruck[" << this << "]" << endl;
@@ -94,9 +123,13 @@ namespace Inheritance
 
 	void CarTestMain()
 	{
-		CBike cBike(1);
-		CAutoBike cAutoBike(2,100);
-		CBus cBus(24,5000);
-		CTruck cTruck(2,3000,1000);
+		//CBike cBike(1);
+		//CVihicle cBike(1);
+		CAutoBike cAutoBike(2, 100); //멤버가 추가되지않으므로 의미없는 상속이다.
+		//CEnginer cAutoBike(2, 100);//엔진을 가진것으로 오토바이를 만든다.
+		//CBus cBus(24,5000); 
+		//CAutoBike cBus(24, 500);//오토바이로 버스를 만드는것은 개념상 문제가 있다.
+		//CEnginer cBus(24, 500);//엔진을 가진것으로 버스를 만드는것은 이상하지않음.
+		//CTruck cTruck(2,3000,1000);
 	}
 }
