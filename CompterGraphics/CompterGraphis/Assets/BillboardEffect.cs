0d7b339450d4597678614b5c31f7e59ee35a4def
downkhg <downkhg@gmail.com>
2021-11-16 20:00:44 +0900

    유니티 빌보드 만들기

---
+++ CompterGraphics/CompterGraphis/Assets/BillboardEffect.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BillboardEffect : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Camera m_camera;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.LookAt(m_camera.transform);
+    }
+}
