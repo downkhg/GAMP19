a53d1cbbc7396613c40c8a87acbf49ed195fd8b5
downkhg <downkhg@gmail.com>
2021-11-19 19:08:33 +0900

    유니티로배우는 수학(백터,쿼터니언,행렬)

---
+++ CompterGraphics/CompterGraphis/Assets/Scripts/Dynamic.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dynamic : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //transform.position += Vector3.forward;
+        //transform.position += Vector3.back;
+
+        //1,3인칭에서는 회전한 케릭터가 바라보는 방향으로 이동한다.
+        //그러므로, 로컬좌표를 이용하여 이동한다.
+        if(Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime);
+            //transform.position += Vector3.forward * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(Vector3.forward * Time.deltaTime);
+            //transform.position += Vector3.back * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Rotate(Vector3.up);
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Rotate(Vector3.down);
+        }
+    }
+}
---
+++ CompterGraphics/CompterGraphis/Assets/Scripts/RotatTarger.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatTarger : MonoBehaviour
+{
+    public GameObject objTarget;
+    public float fDist;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 vTargetPos = objTarget.transform.position;
+        Vector3 vCamPos = this.transform.position;
+
+        Vector3 vDist = vCamPos - vTargetPos;
+        Vector3 vDir = vDist.normalized;
+        Quaternion qRot = Quaternion.Euler(Vector3.up);
+        Vector3 vRotDir = qRot * vDir;
+
+        vCamPos = vTargetPos + vRotDir * fDist;
+
+        transform.LookAt(objTarget.transform);
+        this.transform.position = vCamPos;
+    }
+}
