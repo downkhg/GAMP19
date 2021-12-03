91387d761b1af2b3eb60fb053aa7db34a50a3721
downkhg <downkhg@gmail.com>
2021-12-03 19:45:37 +0900

    아이템추가하기

---
+++ CompterGraphics/CompterGraphis/Assets/Scripts/ItemObejct.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemObejct : MonoBehaviour
+{
+    [SerializeField]
+    Item m_cItem;
+    public Item Item { get { return m_cItem; } set { m_cItem = value; } }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
